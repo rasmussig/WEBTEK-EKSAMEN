7929cc99f3e77819560fb96f9f374c670ca5cf3d
rasmussig <rasmus2001@live.dk>
2023-06-01 22:00:20 +0200

    Yes

---
+++ EksamensEksempler/js_sum_funktion/app.js
@@ -0,0 +1,10 @@
+function sum(a) {
+    let resultat = 0;
+    for (let i = 0; i < a.length; i++)
+         resultat += a[i];
+    return resultat;
+ }
+
+ const a = [1,2,3,4,5]
+
+ console.log(sum(a))
