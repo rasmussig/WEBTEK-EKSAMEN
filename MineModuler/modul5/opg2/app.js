459fac1a803dbad5093d9baa7a1f6d0ed03ee35a
rasmussig <rasmus2001@live.dk>
2023-06-11 23:19:41 +0200

    Yeees

--- MineModuler/modul5/opg2/app.js
+++ MineModuler/modul5/opg2/app.js
@@ -4,7 +4,7 @@
 function sumTalrække(a, b) {
     let sum = 0;
     for (let i = a; i <= b; i++)
-    sum = sum + i;
+    sum = sum + i;  //eller ved brug af sum += i 
     return sum;  }
   console.log(sumTalrække(1,3)); // Skal udskrive 6
   console.log(sumTalrække(2,5)); // Skal udskrive 14
