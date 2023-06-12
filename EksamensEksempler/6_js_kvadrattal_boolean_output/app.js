9f17131cc8f3f725ae87f98b18091c3d92e1f4f7
rasmussig <rasmus2001@live.dk>
2023-06-12 14:16:33 +0200

    Yes

--- EksamensEksempler/6_js_kvadrattal_boolean_output/app.js
+++ EksamensEksempler/6_js_kvadrattal_boolean_output/app.js
@@ -4,7 +4,23 @@ function erÆgteKvadrattal(tal) {
     return Number.isInteger(kvadratrod);
 }
 
+
+//Kan også laves sådan
+function kvardttal(tal) {
+    let x = Math.sqrt(tal);
+    if (x % 1 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 console.log(erÆgteKvadrattal(121)); // Output: true
 console.log(erÆgteKvadrattal(50)); // Output: false
 console.log(erÆgteKvadrattal(16)); // Output: true
 console.log(erÆgteKvadrattal(10)); // Output: false
+
+console.log(kvardttal(121)); // Output: true
+console.log(kvardttal(50)); // Output: false
+console.log(kvardttal(16)); // Output: true
+console.log(kvardttal(10)); // Output: false
