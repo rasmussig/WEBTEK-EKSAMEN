356c11945a6b6b6d04bf02a57b3007cb4e38610b
rasmussig <rasmus2001@live.dk>
2023-06-12 14:11:28 +0200

    Ye

--- EksamensEksempler/11_js_datasæt_output/app.js
+++ EksamensEksempler/11_js_datasæt_output/app.js
@@ -4,12 +4,15 @@ let dataset = [{ name: "Computer", weight: 1.4, price: 4300 },
                 { name: "chair", weight: 17.2, price: 3150 },
                 { name: "light", weight: 1.8, price: 350 }];
 
+            
+//OPG A
 for (let i = 0; i < dataset.length; i++) {
     if (dataset[i].weight > 8) {
         console.info(dataset[i].name); // Udskriver navnet på de objekter, hvis vægt er større end 8
     }
 }
 
+// OPB B
 function getItemsByMaxTotalWeight(data, maxTotalWeight) {
     let currentTotalWeight = 0; // Variabel til at holde styr på den aktuelle samlede vægt
     const result = []; // Array til at gemme de objekter, der opfylder kriteriet
@@ -27,4 +30,16 @@ function getItemsByMaxTotalWeight(data, maxTotalWeight) {
     return result; // Returnerer resultatet af objekter, der opfylder kriteriet
 }
 
+
+// FIND DET OBJEKT SOM VEJER MEST, IKKE EN OPGAVE
 console.log(getItemsByMaxTotalWeight(dataset, 6.0)); // Udskriver objekter med en samlet vægt højst 6.0
+
+let maxWeightObjekt = dataset[0];
+
+for (let i = 1; i < dataset.length; i++) {
+    if (dataset[i].weight > maxWeightObjekt.weight) {
+        maxWeightObjekt = dataset[i];
+    }
+}
+
+console.log("Objekt med maksimal vægt:", maxWeightObjekt.name);
